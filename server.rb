bb89fa5714792af345c1871196fd3c0866cb08a7
Be <bebirchall@gmail.com>
2017-03-13 11:49:31 -0400

    Server: parse incoming message, add connecting client to client list

--- server.rb
+++ server.rb
@@ -1,39 +1,74 @@
 require 'socket'
 require 'json'
 
-class InterruptChat
+class InterruptServer
+
+	CHAT_LENGTH = 45 # length of chat text user can see at one time
+	TICK_LENGTH = 3 # how many seconds to wait before 'moving chat text left'
+	MAX_MSG_LENGTH = 1024 # max length of incoming message read
 	
 	def initialize(host, port)
 		@server = UDPSocket.new
 		@server.bind(host, port)
 
 		@clients = {}
-		@chat_update = nil # hash or nil
-		@outbox = [] # array of json strings
+		@chat_text = ' ' * CHAT_LENGTH
+		@outbox = [] # array of messages, as hashes with sender info
 	end
 
 	def run
-		data, sender = @server.recvfrom(1024)
-		handle_incoming(data, sender)
-		handle_outgoing
+		loop do
+			incoming = IO.select([@server], nil, nil, TICK_LENGTH)
+			if incoming.nil?
+				tick
+			else
+				data, sender = @server.recvfrom(MAX_MSG_LENGTH)
+				handle_incoming(data, sender)
+			end
+			handle_outgoing
+		end
+	end
+
+	def tick
+		if @chat_text != ' ' * CHAT_LENGTH
+			update_chat_text(' ')
+		end
 	end
 
 	def handle_incoming(data, sender)
 		msg = parse_msg(data)
 		return if msg == nil
 
-		key = get_key(sender)
+		key, host, port = sender_info(sender)
 
-
+		case msg['type']
+		when 'connect'
+			add_client(key, host, port, msg['name'], msg['time'])
+			ack_client(host, port)
+		when 'quit'
+			delete_client(key)
+		when 'chat'
+			if @clients.has_key? key and new_msg?(key, msg['time'])
+				update_time(key, msg['time'])
+				update_chat_text(msg['body'])
+			end
+		end
 	end
 
-	def handle_outgoing
+	def handle_outbox
+		while not @outbox.empty?
+			msg = @outbox.shift
 
+		end
 	end
 
 
-	def get_key(sender)
-		sender[1] + sender[2].to_s # host + port is unique per client
+	def sender_info(sender)
+		port = sender[1]
+		host = sender[2]
+		key = port.to_s + host # host + port is unique per client
+
+		[key, host, port]
 	end
 
 	# parse to json, and ensure formatted correctly
@@ -49,14 +84,14 @@ class InterruptChat
 
 	# return nil if fields wrong, otherwise return msg
 	def check_fields(msg)
-		basic_fields? = msg.has_key? 'type' && msg.has_key? 'time'
-		return nil if not basic_fields?
+		basic_fields = (msg.has_key?('type') && msg.has_key?('time'))
+		return nil if not basic_fields
 
 		case msg['type']
 		when 'connect'
-			has_keys = msg.has_key? 'name'
+			return nil if not msg.has_key?('name')
 		when 'chat'
-			has_keys = msg.has_key? 'body'
+			return nil if not msg.has_key?('body')
 		when 'quit'
 			
 		else
@@ -66,5 +101,56 @@ class InterruptChat
 		return msg
 	end
 
+	def add_client(key, host, port, nickname, time)
+		@clients[key] = {
+			'key' => key,
+			'name' => nickname,
+			'time' => time,
+			'host' => host,
+			'port' => port
+		}
+	end
+
+	def delete_client(key)
+		@clients.delete(key)
+	end
+
+	def update_time(key, time)
+		@clients[key]['time'] = time
+	end
+
+	def new_msg?(key, time)
+		@clients[key]['time'] <= time
+	end
+
+	def update_chat_text(string) # assumes string shorter than CHAT_LENGTH
+		@chat_text = @chat_text[string.length..-1] + string
+		msg = {
+			'type' => 'chat',
+			'msg' => @chat_text
+		}
+
+		@outbox << @chat_text
+	end
+
+	def ack_client(host, port)
+		msg = {
+			'type' => 'private_new',
+			'host' => host,
+			'port' => port,
+			'msg' => {'type' => 'ack'}
+		}
+
+		@outbox << msg
+	end
+
+	def send_msg(msg, host, port)
+		msg['time'] = Time.to_f.to_s
+		json = msg.to_json
+		@server.send(json, 0, host, port)
+	end
+
 end
 
+server = InterruptServer.new('localhost', 4481)
+server.run
