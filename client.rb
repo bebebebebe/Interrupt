355c0d99eb0311d9b3f3d205fe4562fdf3c64f84
Be <bebirchall@gmail.com>
2017-03-13 16:01:33 -0400

    Client: connect handshake, parse incoming messages, checking format/sender

--- client.rb
+++ client.rb
@@ -1,17 +1,26 @@
 require 'socket'
 require 'json'
 
-class InterruptChat
+class InterruptClient
 
-	def initialize
+	START_MSG = 'Welcome! What is your name?'
+	PROMPT = '> '
+	MAX_MSG_LENGTH = 1024 # max length of incoming message read
+	HANDSHAKE_WAIT = 2 # number of seconds to wait for ack from server before resending
+
+
+	def initialize(server_host, server_port)
+		@server_host = server_host
+		@server_port = server_port
+
 		@client = UDPSocket.new
-		@client.connect('localhost', 4481)
+		@client.connect(@server_host, @server_port)
 	end
 
 	def run
 		set_name
-		announce
-		terminal_config
+		handshake
+		#terminal_config
 		receive_loop
 	end
 
@@ -21,16 +30,77 @@ class InterruptChat
 		@name = gets.chomp
 	end
 
-	def announce
-		send_msg(msg_connect(@name))
+	def handshake
+		ackd = false
+		while not ackd
+			send_msg(msg_connect(@name))
+			incoming = IO.select([@client], nil, nil, HANDSHAKE_WAIT)
+			if ((not incoming.nil?) && ack?)
+				ackd = true
+			end
+		end
+	end
+
+	def ack?
+		data, sender = @client.recvfrom(MAX_MSG_LENGTH)
+		msg = parse_msg(data, sender)
+		return false if msg.nil?
+
+		msg['type'] == 'ack'
 	end
 
 	def receive_loop
+		loop do
+			to_read = IO.select([@client, STDIN]) # check what's ready to read: socket, or terminal input
+			to_read[0].each { |ios|
 
+				if ios == @client
+					msg, sender = ios.recvfrom(MAX_MSG_LENGTH)
+					p msg
+					bye
+				end
+			}
+		end
 	end
 
+	# return parsed msg if correct format and comes from Interrupt server
+	# return nil otherwise
+	def parse_msg(data, sender)
+		return nil if not is_server?(sender)
+
+		begin
+			msg = JSON.parse(data)
+			msg = check_fields(msg)
+		rescue
+			nil
+		end
+	end
+
+	def is_server?(sender)
+		port = sender[1]
+		host = sender[2]
+
+		(port == @server_port) && (host == @server_host)
+	end
+
+	# return nil if fields wrong, otherwise return msg
+	def check_fields(msg)
+		basic_fields = (msg.has_key?('type') && msg.has_key?('time'))
+		return nil if not basic_fields
+
+		case msg['type']
+		when 'chat'
+			msg if msg.has_key?('body')
+		when 'ack'
+			msg
+		else
+			nil
+		end
+	end
+
+
 	def send_msg(msg)
-		msg[time] = Time.now.to_f.to_s
+		msg['time'] = Time.now.to_f.to_s
 		json = msg.to_json
 		@client.send(json, 0)
 	end
@@ -62,3 +132,11 @@ class InterruptChat
 	end
 
 end
+
+
+
+SERVER_HOST = '127.0.0.1'
+SERVER_PORT = 4481
+
+client = InterruptClient.new(SERVER_HOST, SERVER_PORT)
+client.run
